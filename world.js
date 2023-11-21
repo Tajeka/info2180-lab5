727317a12779209ab6801d622158082d3953b77c
Tajeka <johnsontajeka@gmail.com>
2023-11-20 20:22:33 -0500

    Edited world.js

--- world.js
+++ world.js
@@ -13,4 +13,21 @@ document.addEventListener("DOMContentLoaded", function () {
           console.error(error);
         });
     });
+  
+    // city
+  
+    document.getElementById("city").addEventListener("click", function () {
+      var country = encodeURIComponent(document.getElementById("country").value);
+  
+      fetch("world.php?country=" + country + "&lookup=cities")
+        .then((response) => {
+          return response.json();
+        })
+        .then((res) => {
+          document.getElementById("result").innerHTML = res;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
   });
