09ea0561dd3242e35d072aa92da131dad3c95593
Tajeka <johnsontajeka@gmail.com>
2023-11-20 19:49:03 -0500

    Edited world.js

---
+++ world.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("lookup").addEventListener("click", function () {
+      var country = encodeURIComponent(document.getElementById("country").value);
+  
+      fetch("world.php?country=" + country)
+        .then((response) => {
+          return response.json();
+        })
+        .then((res) => {
+          document.getElementById("result").innerHTML = res;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  });
