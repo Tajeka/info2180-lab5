38995339b8c7036ea6215208aab518dce25161ae
Tajeka <johnsontajeka@gmail.com>
2023-11-20 22:36:28 -0500

    Edited world.js

--- world.js
+++ world.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("lookup").addEventListener("click", function () {
-      var country = encodeURIComponent(document.getElementById("country").value);
-  
-      fetch("world.php?country=" + country)
-        .then((response) => {
-          return response.json();
-        })
-        .then((res) => {
-          document.getElementById("result").innerHTML = res;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    });
-  
-    // city
-  
-    document.getElementById("city").addEventListener("click", function () {
-      var country = encodeURIComponent(document.getElementById("country").value);
-  
-      fetch("world.php?country=" + country + "&lookup=cities")
-        .then((response) => {
-          return response.json();
-        })
-        .then((res) => {
-          document.getElementById("result").innerHTML = res;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    });
+  document.getElementById("lookup").addEventListener("click", function () {
+    var country = encodeURIComponent(document.getElementById("country").value);
+
+    fetch("world.php?country=" + country)
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        document.getElementById("result").innerHTML = res;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   });
+
+  // city
+
+  document.getElementById("city").addEventListener("click", function () {
+    var country = encodeURIComponent(document.getElementById("country").value);
+
+    fetch("world.php?country=" + country + "&lookup=cities")
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        document.getElementById("result").innerHTML = res;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+});
