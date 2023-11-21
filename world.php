3afc7606e920b307b8089b064e11ff788fa29ccd
Tajeka <johnsontajeka@gmail.com>
2023-11-20 19:51:58 -0500

    Edited world.php

--- world.php
+++ world.php
@@ -1,17 +1,29 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Check if 'country' parameter is set
+if(isset($_GET['country'])) {
+     
+    $raw_country = $_GET['country'];
+    $country = htmlspecialchars($raw_country, ENT_QUOTES, 'UTF-8');
 
+    // Fetch data for the specified country
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+
+    $country_list = '<ul>';
+    foreach ($results as $row) {
+        $country_list .= '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    $country_list .= '</ul>';
+
+    echo json_encode($country_list);
+    exit;
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
